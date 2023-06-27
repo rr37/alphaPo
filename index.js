7748d0faeca19843c03e010edd15b454082b1b58
rr37 <ay870527@gmail.com>
2023-06-27 12:10:35 +0800

    feat: add Drink constructor function

---
+++ index.js
@@ -0,0 +1,24 @@
+// Constructor function for Drinks
+function Drink (name, sugar, ice) {
+  this.name = name
+  this.sugar = sugar
+  this.ice = ice
+}
+
+Drink.prototype.price = function () {
+  switch (this.name) {
+    case 'Black Tea':
+    case 'Oolong Tea':
+    case 'Baozong Tea':
+    case 'Green Tea':
+      return 30
+    case 'Bubble Milk Tea':
+    case 'Lemon Green Tea':
+      return 50
+    case 'Black Tea Latte':
+    case 'Matcha Latte':
+      return 55
+    default:
+      alert('No this drink')
+  }
+}
