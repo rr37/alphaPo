a0c15e1cee5de75ab75d16ab4d917d4bbbe64b21
rr37 <ay870527@gmail.com>
2023-06-27 13:00:45 +0800

    feat: add add order function

--- index.js
+++ index.js
@@ -22,3 +22,57 @@ Drink.prototype.price = function () {
       alert('No this drink')
   }
 }
+
+// new the alphaPos Instance
+const alphaPos = new AlphaPos()
+
+const addDrinkButton = document.querySelector('[data-alpha-pos="add-drink"]')
+addDrinkButton.addEventListener('click', function () {
+  // 1. 取得店員選擇的飲料品項、甜度和冰塊
+  const drinkName = alphaPos.getCheckedValue('drink')
+  const ice = alphaPos.getCheckedValue('ice')
+  const sugar = alphaPos.getCheckedValue('sugar')
+  // 2. 如果沒有選擇飲料品項，跳出提示
+  if (!drinkName) {
+    alert('Please choose at least one item.')
+    return
+  }
+  // 3. 建立飲料實例，並取得飲料價格
+  const drink = new Drink(drinkName, sugar, ice)
+  // 4. 將飲料實例產生成左側訂單區的畫面
+  alphaPos.addDrink(drink)
+})
+
+
+// Constructor function for Alpha Pos System
+function AlphaPos () { }
+AlphaPos.prototype.getCheckedValue = function (inputName) {
+  let selectedOption = ''
+  document.querySelectorAll(`[name=${inputName}]`).forEach(function(item) {
+    if (item.checked) {
+      selectedOption = item.value
+    }
+  })
+  return selectedOption
+}
+
+const orderLists = document.querySelector('[data-order-lists]') 
+AlphaPos.prototype.addDrink = function (drink) {
+  let orderListsCard = `
+    <div class="card mb-3">
+    <div class="card-body pt-3 pr-3">
+      <div class="text-right">
+        <span data-alpha-pos="delete-drink">×</span>
+      </div>
+      <h6 class="card-title mb-1">${drink.name}</h6>
+      <div class="card-text">${drink.ice}</div>
+      <div class="card-text">${drink.sugar}</div>
+    </div>
+    <div class="card-footer text-right py-2">
+      <div class="card-text text-muted">$ <span data-drink-price>${drink.price()}</span></div>
+    </div>
+  </div>
+  `
+
+  orderLists.insertAdjacentHTML('afterbegin', orderListsCard)
+}
